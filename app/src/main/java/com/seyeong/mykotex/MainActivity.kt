2c1cf3a23fa78c8204f2fae99e22e44435ddea68
ys200209 <lys200209@naver.com>
2021-07-07 01:02:18 +0900

    View Binding Test

--- app/src/main/java/com/seyeong/mykotex/MainActivity.kt
+++ app/src/main/java/com/seyeong/mykotex/MainActivity.kt
@@ -2,10 +2,21 @@ package com.seyeong.mykotex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.seyeong.mykotex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+            binding.textView.setText("Hello Kotlin~!")
+        }
+
+
+
     }
 }
